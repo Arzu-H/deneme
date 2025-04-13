3714635f47fbd614c71f4a37a0e6282aa1371853
Arzu-H <162874561+Arzu-H@users.noreply.github.com>
2025-04-13 19:08:03 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "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")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
 COCKTAIL_CSV_PATH = r"final_cocktails.csv"
 PREFERENCES_FILE = "user_preferences.csv"
